ea77ceabb4af8c6af4f137d4b923deb191afeb25
Zouine2005 <zouinemohamade@gmail.com>
2024-07-20 23:44:52 +0100

    add the chat and add firend options in nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('chat.index')" :active="request()->routeIs('chat')">
+                        {{ __('Chat') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('users')" :active="request()->routeIs('users')">
+                        {{ __('Add friend') }}
+                    </x-nav-link>
                 </div>
             </div>
 
